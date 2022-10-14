6e2def5ca758c8738fb066187c0ac217ba75e7d8
philly <phillychi3@gmail.com>
2022-10-15 03:30:36 +0800

    add camera

--- camera.py
+++ camera.py
@@ -2,54 +2,62 @@ import cv2
 import threading
 import numpy as np
 from PIL import Image, ImageFilter , ImageDraw , ImageFont
+import time
 CASC_PATH = 'haarcascade_frontalface_default.xml'
 font = ImageFont.truetype('GenWanMin-L.ttc', 20)
-from detectface import test2
+
+cv2.useOptimized()
+video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cascade_classifier = cv2.CascadeClassifier(CASC_PATH)
+def face_detect(image):
+    faces = cascade_classifier.detectMultiScale(
+        image,
+        scaleFactor=1.1,
+        minNeighbors=5,
+    )
+    if not len(faces) > 0:
+        return None
+    else:
+        return faces
 
 
-class ipcamCapture:
-    def __init__(self, URL):
-        self.Frame = []
-        self.status = False
-        self.isstop = False
-		
-	#攝影機連接。
-        self.video_capture =cv2.VideoCapture(0, cv2.CAP_DSHOW)
-        #self.video_capture = cv2.VideoCapture(URL)
-    def start(self):
-	#把程式放進子執行緒
-        print('ipcam started!')
-        threading.Thread(target=self.queryframe, daemon=True, args=()).start()
+def detect():
+    while True:
+        if not video_capture.isOpened():
+            print('找不到相機')
+            pass
+        ret, frame = video_capture.read()
+        grayimage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_detect(grayimage)
+        if faces is not None:
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 0)
+                pliimage = Image.fromarray(frame)
+                blurimage = pliimage.crop((x,y,x+w,y+h))
+                blurimage = blurimage.filter(ImageFilter.GaussianBlur(radius=10))
+                im = Image.new('RGB', (pliimage.size[0], pliimage.size[1]), (255, 255, 255, 0))
+                im.paste(pliimage, (0, 0))
+                im.paste(blurimage, (x, y))
+                imdraw = ImageDraw.Draw(im)
+                text = '此圖違反數位中介法'
+                w, _ = imdraw.textsize(text, font=font)
+                imdraw.text((x+(w-w)/2, y+h/2), text, fill=(255, 255, 255),font=font)
+                feame = np.array(im)
+                
 
-    def stop(self):
-	#停止無限迴圈的開關。
-        self.isstop = True
-        print('ipcam stopped!')
-   
-    def getframe(self):
-	#再回傳最新的影像。
-        return self.Frame
-        
-    def queryframe(self):
-        while (not self.isstop):
-            self.status, self.Frame = self.video_capture.read()
-        
-        self.video_capture.release()
-
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        cv2.imshow('Video', frame)
 
 
 
 
-ipcam = ipcamCapture(0)
-ipcam.start()
 
 
-while True:
-    frame = ipcam.getframe()
-    frame = test2(frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        ipcam.stop()
-        cv2.destroyAllWindows()
-        break
-    cv2.imshow('Video', frame)
+
+if __name__ == '__main__':
+    detect()
+    video_capture.release()
+    cv2.destroyAllWindows()
