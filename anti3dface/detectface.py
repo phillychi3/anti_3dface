9efacb73797eca9e64629b79c70021e776c3b8bf
philly <phillychi3@gmail.com>
2022-10-16 15:17:31 +0800

    finish

--- anti3dface/detectface.py
+++ anti3dface/detectface.py
@@ -23,7 +23,7 @@ def _face_detect(image):
 
 
 def _addimage(image, pos, size):
-    pilimage = Image.open(image)
+    pilimage = Image.fromarray(image)
     blurimage = pilimage.crop((pos[0], pos[1], pos[0]+size[0], pos[1]+size[1]))
     blurimage = blurimage.filter(ImageFilter.GaussianBlur(radius=10))
     im = Image.new(
@@ -31,7 +31,7 @@ def _addimage(image, pos, size):
     im.paste(pilimage, (0, 0))
     im.paste(blurimage, (pos[0], pos[1]))
     imdraw = ImageDraw.Draw(im)
-    text = '此圖違反數位中介法'
+    text = '違反數位中介法'
     w, _ = imdraw.textsize(text, font=font)
     imdraw.text((pos[0]+(size[0]-w)/2, pos[1]+size[1]/2),
                 text, fill=(255, 255, 255), font=font)
@@ -42,16 +42,13 @@ def detect_image(image):
     image = cv2.imread(image)
     grayimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = _face_detect(grayimage)
-    print(faces)
     x = faces[0][0]
     y = faces[0][1]
     w = faces[0][2]
     h = faces[0][3]
     # for (x, y, w, h) in faces:
     #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 0)
-    cv2.imwrite("head.jpg", image)
-    print(x, y)
-    im = _addimage("head.jpg", (x, y), (w, h))
-    imgbytes = io.BytesIO()
-    im.save(imgbytes, format='jpg')
-    return imgbytes.getvalue()
+    # change image color to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    im = _addimage(image, (x, y), (w, h))
+    return im
